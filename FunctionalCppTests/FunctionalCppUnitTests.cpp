596c23cc427882c045c5c6bb797e47ad45cf1d61
Dom Musgrave <imdomdude@gmail.com>
2015-07-30 21:55:39 +0100

    Stole Scalas Val type
    Const on const on const on const

--- FunctionalCppTests/FunctionalCppUnitTests.cpp
+++ FunctionalCppTests/FunctionalCppUnitTests.cpp
@@ -5,36 +5,35 @@
 #include <FunctionalCpp.hpp>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
-using namespace Functional;
+using namespace FunctionalCPP;
 
 namespace FunctionalCppTests
 {		
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		int testOp(int x, int y, Func<int(int, int)> op)
+		int testOp(val<int> x, val<int> y, Func<val<int>(val<int>, val<int>)> op)
 		{
 			return op(x, y);
 		}
 
-		static int add(int x, int y) {
+		static val<int> add(val<int> x, val<int> y) {
 			return x + y;
 		}
 		//Tests that a function can be passed into another function
 		TEST_METHOD(HigherOrderFunctionTest)
 		{
-			Func<int(int, int)> localAdd = add;
+			Func<val<int>(val<int>, val<int>)> localAdd = add;
 			auto expected = add(1, 1);
 			auto result = testOp(1, 1, localAdd);
-			
+
 			Assert::AreEqual(expected, result);
 		}
 
 		//Lambdas can be passed into another function
 		TEST_METHOD(LambdasHigherOrderFunctionTest)
 		{
-			Func<int(int, int)> myAdd = [](int x, int y) { return x + y;  };
+			Func<val<int>(val<int>, val<int>)> myAdd = [](val<int> x, val<int> y) { return x + y;  };
 			auto expected = 1 + 1;
 			auto result = testOp(1, 1, myAdd);
 
@@ -45,9 +44,9 @@ namespace FunctionalCppTests
 		//maps a function onto a set of values
 		TEST_METHOD(MapValueOnToCollection)
 		{
-			std::vector<int> myInts{ 1, 2, 3, 4, 5 };
-
-			auto newInts = map<int>(myInts, [](int x){ return x*x; });
+			std::vector<val<int>> myInts{ 1, 2, 3, 4, 5 };
+			Functional F;
+			auto newInts = F.map<val<int>>(myInts, [](val<int> x){ return x*x; });
 
 			std::vector<int> expectedInts{ 1, 4, 9, 16, 25 };
 
@@ -56,6 +55,5 @@ namespace FunctionalCppTests
 				Assert::AreEqual(newInts[i], expectedInts[i]);
 			}
 		}
-
 	};
 }
--- fCpp/FunctionalCpp.hpp
+++ fCpp/FunctionalCpp.hpp
@@ -2,20 +2,27 @@
 #include <functional>
 #include <algorithm>
 
-namespace Functional {
+namespace FunctionalCPP {
+	template<typename T>
+	using val = const T;
 
 	template<typename T>
 	using Func = std::function < T > ;
 
-	template<typename T>
-	const std::vector<T> map(const std::vector<T> collection, const Func<T(T)> op) const {
-		std::vector<T> copy;
+	class Functional {
+	public :
+		Functional() {};
 
-		for (auto iter = collection.begin(); iter != collection.end(); iter++) {
-			copy.push_back(op((*iter)));
-		}
+		template<typename T>
+		const std::vector<T> map(const std::vector<T> collection, const Func<T(T)> op) const {
+			std::vector<T> copy;
 
-		return copy;
-	}
+			for (auto iter = collection.begin(); iter != collection.end(); iter++) {
+				copy.push_back(op((*iter)));
+			}
+
+			return copy;
+		};
+	};
 
 }
